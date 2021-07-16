64b8889b0b6d44264317435cec864d4e691f6bc4
PAPALAND <eve@papaland.life>
2021-07-16 16:29:41 +0800

    fist commit

---
+++ src/utils/computePoolAddress.ts
@@ -0,0 +1,39 @@
+import { defaultAbiCoder } from '@ethersproject/abi'
+import { getCreate2Address } from '@ethersproject/address'
+import { keccak256 } from '@ethersproject/solidity'
+import { Token } from '@uniswap/sdk-core'
+import {
+  FeeAmount,
+  POOL_INIT_CODE_HASH,
+  POOL_INIT_CODE_HASH_OPTIMISM,
+  POOL_INIT_CODE_HASH_OPTIMISM_KOVAN
+} from '../constants'
+
+export function computePoolAddress({
+  factoryAddress,
+  tokenA,
+  tokenB,
+  fee,
+  initCodeHashManualOverride
+}: {
+  factoryAddress: string
+  tokenA: Token
+  tokenB: Token
+  fee: FeeAmount
+  initCodeHashManualOverride?: string
+}): string {
+  const [token0, token1] = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA] // does safety checks
+  return getCreate2Address(
+    factoryAddress,
+    keccak256(
+      ['bytes'],
+      [defaultAbiCoder.encode(['address', 'address', 'uint24'], [token0.address, token1.address, fee])]
+    ),
+    initCodeHashManualOverride ??
+      (tokenA.chainId === 10
+        ? POOL_INIT_CODE_HASH_OPTIMISM
+        : tokenA.chainId === 69
+        ? POOL_INIT_CODE_HASH_OPTIMISM_KOVAN
+        : POOL_INIT_CODE_HASH)
+  )
+}
